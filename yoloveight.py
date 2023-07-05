09d0ec0263c1ae909f3edaef0943f56886de5494
saktheeswaranswan <bolacalculus@gmail.com>
2023-07-05 16:45:54 +0530

    yolov8

---
+++ yoloveight.py
@@ -0,0 +1,78 @@
+import csv
+import time
+import cv2
+import supervision as sv
+from ultralytics import YOLO
+
+
+def main():
+    # to save the video
+    writer = cv2.VideoWriter('webcam_yolo.mp4',
+                             cv2.VideoWriter_fourcc(*'DIVX'),
+                             7,
+                             (1280, 720))
+
+    # define resolution
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    # specify the model
+    model = YOLO("yolov8n.pt")
+
+    # customize the bounding box
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1
+    )
+
+    # Create a CSV file to log the detections
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    csv_file = open(f"detections_{timestamp}.csv", "w", newline="")
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["Label", "Confidence", "Timestamp", "Crop"])
+
+    while True:
+        ret, frame = cap.read()
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_yolov8(result)
+        labels = [
+            f"{model.model.names[class_id]} {confidence:0.2f}"
+            for _, confidence, class_id, _
+            in detections
+        ]
+        frame = box_annotator.annotate(
+            scene=frame,
+            detections=detections,
+            labels=labels
+        )
+
+        # Log detections in CSV file and save cropped images
+        for detection, label in zip(detections, labels):
+            _, confidence, class_id, box = detection
+            label_name = model.model.names[class_id]
+            csv_writer.writerow([label_name, confidence, timestamp, box])
+
+            # Crop the detection region
+            x, y, w, h = box
+            crop = frame[y:y + h, x:x + w]
+            cv2.imwrite(f"{label_name}_{timestamp}.jpg", crop)
+
+        writer.write(frame)
+        cv2.imshow("yolov8", frame)
+
+        if cv2.waitKey(30) == 27:  # break with escape key
+            break
+
+    # Close the CSV file
+    csv_file.close()
+
+    cap.release()
+    writer.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ yoloveightnew.py
@@ -0,0 +1,12 @@
+Here's a list of the libraries required for the modified code:
+
+OpenCV: Used for video capturing and processing.
+
+Install with pip: pip install opencv-python
+Supervision: Used for annotation.
+
+Install with pip: pip install supervision
+Ultralytics: Used for the YOLO model.
+
+Install with pip: pip install ultralytics
+Make sure to install these libraries before running the code.
